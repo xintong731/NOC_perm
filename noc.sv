023005ece3cedc5b98ee0e3b38946568cdd417f7
xintong731 <56935104+xintong731@users.noreply.github.com>
2020-10-16 03:30:08 -0700

    Update noc.sv

--- noc.sv
+++ noc.sv
@@ -1,139 +1,196 @@
 module noc_intf(input clk, input reset, 
 	 			input noc_to_dev_ctl, input[7:0] noc_to_dev_data,
-	 			input stopin, input pushout, input firstout,
-	 			output reg noc_from_dev_ctl, output reg [7:0] noc_from_dev_data,
-	 			output reg pushin, output reg firstin, output reg stopout)
+				output reg noc_from_dev_ctl, output reg [7:0] noc_from_dev_data,
+	 			output reg pushin, output reg firstin, input stopin,
+				output reg [63:0] din, input pushout, input firstout,
+				output stopout, input [63:0] dout);
 
 integer count_id, count_id_d;//count packet operation
 integer count_addr,count_addr_d;//count addr
 integer count_data, count_data_d;//count data
 integer count_din, count_din_d;//count 64bit data go to perm
+integer t_count,t_count_d;
 reg[63:0] noc_din;
-reg[1:0] w_alen,r_alen;
-reg[2:0] w_dlen,r_dlen;
-reg[63:0] w_addr,r_addr;
-reg[7:0] w_des_id, w_sor_id, r_des_id, r_sor_id;
-reg[127:0]w_data;
-reg[1:0]RC;
+reg[1:0] t_alen,t_alen_d, b_alen,b_alen_d;
+reg[2:0] t_dlen,t_dlen_d,b_dlen,b_dlen_d;
+reg[7:0][7:0] t_addr,t_addr_d,b_addr,b_addr_d;
+reg[7:0] t_des_id, t_des_id_d,t_sor_id,t_sor_id_d, b_des_id,b_des_id_d, b_sor_id,b_sor_id_d;
+reg[7:0] t_data,t_data_d;
+reg[1:0]RC,RC_d;
+reg[7:0]actual_t_dlen, actual_t_dlen_d;
+reg [2:0] mes, mes_d;
+reg[7:0][7:0]din_temp,din_temp_d;
+reg [3:0]mes_b,mes_b_d;
+reg[7:0] stop_addr,stop_addr_d;//stopin from 1 to 0, send message
+reg[7:0] stop_data,stop_data_d;
 
-
+enum reg[2:0]{//transfer
+	T_IDLE,
+	NOP,
+	Read,
+	Write,
+	Perm_write
+}t_cs, t_ns;
 
 always @(posedge clk or posedge reset)begin
 	if(reset)begin
-		//cs<=NOP;
-		noc_to_dev_data[2:0]<=0;
+		t_cs<=T_IDLE;
 		count_id<=0;
-		end else begin
+		count_addr<=0;
+		count_data<=0;
+		t_alen<=0;
+		t_dlen<=0;
+		t_addr<=0;
+		t_des_id<=0;
+		t_sor_id<=0;
+		t_data<=0;
+		RC<=0;
+		actual_t_dlen<=0;
+		mes<=0;
+		din_temp<=0;
+		mes_b<=0;
+	end else begin
+			t_cs<= #1 t_ns;
 			count_id<= #1 count_id_d;
 			count_addr<= #1 count_addr_d;
 			count_data<= #1 count_data_d;
-		end
+			t_count<= #1 t_count_d;
+			t_alen<= #1 t_alen_d;
+			t_dlen<= #1 t_dlen_d;
+			t_addr<= #1 t_addr_d;
+			t_des_id<= #1 t_des_id_d;
+			t_sor_id<= #1 t_sor_id_d;
+			t_data<= #1 t_data_d;
+			RC<= #1 RC_d;
+			actual_t_dlen<= #1 actual_t_dlen_d;
+			mes<= #1 mes_d;
+			din_temp<= #1 din_temp_d;
+			mes_b<= #1 mes_b_d;
+
 	end
 end
 
-always @(*)begin//write
-	case(noc_to_dev_data[2:0])
-		0:begin//NOP
-			w_alen = 0;
-			w_dlen = 0;
-			w_addr = 0;
+
+
+always @(*)begin//tb to noc
+	t_ns = t_cs;
+	count_id_d = count_id;
+	count_addr_d = count_addr;
+	count_data_d = count_data;
+	t_count_d = t_count;
+	t_alen_d = t_alen;
+	t_dlen_d = t_dlen;
+	t_addr_d = t_addr;
+	t_des_id_d = t_des_id;
+	t_sor_id_d = t_sor_id;
+	t_data_d = t_data;
+	mes_d = mes;
+	din_temp_d = din_temp;
+	mes_b_d = mes_b;
+	case(t_cs)
+		T_IDLE:begin
+			t_alen = 0;
+			t_dlen = 0;
+			t_addr = 0;
 			count_id_d = 0;
 			count_addr_d = 0;
 			count_data_d = 0;
-		end
-		2:begin//WRITE
+			t_count_d = 0;
+			flag_d = 0;
 			if(noc_to_dev_ctl)begin
-				w_alen = noc_to_dev_data[7:6];
-				w_dlen = noc_to_dev_data[5:3];
-			end else begin
-			    if(count_id<=2)begin
-					count_id_d = count_id + 1;
-					if(count_id==1)
-						w_des_id = noc_to_dev_data;
-					else if(count_id==2)
-						w_sor_id = noc_to_dev_data;
-				else begin
-					count_id_d = 0;
-					count_addr_d = 0;
-				end
-				if(count_addr<2**w_alen)begin
-					w_addr[8*count_addr + 7: 8*count_addr] = noc_to_dev_data;
-					count_addr_d = count_addr + 1;
-				end else begin
-					count_addr_d = 0;
-					count_data_d = 0;
-				end
-				if(count_data<2**w_dlen)begin
-					w_data[8*count_data + 7: 8*count_data] = noc_to_dev_data;
-					count_data_d = count_data + 1;
-				end else if(count_data<2**w_dlen && (count_data+1)%8==0)begin
-					firstin = 1;
-					pushin = 1;
-					w_data[8*count_data + 7: 8*count_data] = noc_to_dev_data;
-					din = w_data[8*(count_data + 1)-1: 8*(count_data + 1)-64];
-					count_data_d = count_data + 1;
-				end else begin
-					firstin = 0;
-					pushin = 0;
-					noc_from_dev_ctl = 1;
-					
-				end
-						
+				case(noc_to_dev_data[2:0])
+					0:begin
+						t_ns = NOP;
+						mes_d = 0;
+					end
+					1:begin
+						$display("read");
+						t_ns = Read;
+						t_alen_d = noc_to_dev_data[7:6];
+						t_dlen_d = noc_to_dev_data[5:3];
+						$display("ctl %b data %b %0t",noc_to_dev_ctl,noc_to_dev_data,$time);
+						mes_d = 1;
+					end
+					2:begin
+						$display("write");
+						t_ns = Write;
+						t_alen_d = noc_to_dev_data[7:6];
+						t_dlen_d = noc_to_dev_data[5:3];
+						$display("ctl %b data %b %0t",noc_to_dev_ctl,noc_to_dev_data,$time);
+						mes_d = 2;
+					end
+				endcase
 			end
 		end
-		4:begin//WRITE RESPONSE
-			if(noc_from_dev_ctl)begin
-				noc_from_dev_data[5:0] = 6'b000100;
-				noc_from_dev_data[7:6] = RC;
-			end else begin
-			    if(count_id<=2)begin
-					count_id_d = count_id + 1;
-					if(count_id==1)
-						wr_des_id = w_sor_id;
-					else if(count_id==2)
-						wr_sor_id = w_des_id;
-				else begin
-					count_id_d = 0;
-				end
+		Read:begin
+			if(noc_to_dev_ctl==0 && mes==1)begin
+					if(count_id==0)begin
+						t_des_id_d = noc_to_dev_data;
+						count_id_d = count_id + 1;
+					end else if(count_id==1)begin
+						t_sor_id_d = noc_to_dev_data;
+						count_id_d = count_id + 1;
+					end else if(count_id==2 && count_addr<2**t_alen)begin
+						t_addr_d[count_addr] = noc_to_dev_data;
+						count_addr_d = count_addr + 1;
+					end else begin
+						RC=2'b00;
+						mes_b_d = 3;
+					end
+			end	
+		end
+		Write:begin
+			if(noc_to_dev_ctl==0 && mes==2)begin
+					if(count_id==0)begin
+						t_des_id_d = noc_to_dev_data;
+						count_id_d = count_id + 1;
+					end else if(count_id==1)begin
+						t_sor_id_d = noc_to_dev_data;
+						count_id_d = count_id + 1;
+					end else if(count_id==2 && count_addr<2**t_alen)begin
+						t_addr_d[count_addr] = noc_to_dev_data;
+						count_addr_d = count_addr + 1;
+					end else if(count_addr>=2**t_alen && t_count<200 && count_data<2**t_dlen)begin
+						if((count_data+1)%8==0)begin
+							t_ns = Perm_write;
+						end else begin
+							din_temp_d[count_data]= noc_to_dev_data;
+							count_data_d = count_data + 1;
+							t_count_d = t_count + 1;
+							t_ns = Write;
+						end
+					end else if(count_data=2**t_dlen)begin
+						RC_d = 2'b00
+						mes_b_d = 4;
+					end else if(t_count==200 && count_data<2**t_dlen)begin
+						RC_d = 2‘b10;
+						mes_b_d = 4;
 
+					end
+				//end
 			end
 		end
-		5:begin//MESSAGE
-			
-		end
-
-	endcase
-end
-
-
-always @(posedge clk or posedge reset)begin
-	if(reset)begin
-		//cs<=NOP;
-		noc_to_dev_data[2:0]<=0;
-		count_id<=0;
-		end else begin
-			count_id<= #1 count_id_d;
-			count_addr<= #1 count_addr_d;
-			count_data<= #1 count_data_d;
-		end
-	end
-end
-
-
-always @(*)begin
-	case(noc_to_dev_data[2:0])
-		0:begin//NOP
-			r_alen = 0;
-			r_dlen = 0;
-			r_addr = 0;
-			count_id_d = 0;
-			count_addr_d = 0;
-			count_data_d = 0;
-		end
-		1:begin//READ
-			
+		Perm_write:begin
+			if(stopin==0)begin
+				pushin = 1;
+				din = din_temp;
+				din_temp_d = 0;
+				t_ns = Write;
+			end else begin
+				RC_d = 2'b01;
+				mes_b_d = 4;
+				actual_t_dlen_d = 8'h02;
+			end
 		end
 	endcase
 end
 
+always @(*)begin//noc to tb
+	
+end
+
+always @(negedge stopin)begin
+	stop_addr_d = 8'h42;
+	stop_data_d = 8'h78;
+end
 endmodule
